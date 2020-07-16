e06083443d014c25ce9598706ac00404cdebcc3e
Marco <marco@mojica.de>
2020-07-16 17:26:28 +0200

    Add validation to taskfilter

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,10 +1,24 @@
 import { TaskStatus } from '../task.model';
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsOptional,
+  IsEnum,
+  ValidationArguments,
+  IsNotEmpty,
+} from 'class-validator';
 
 export class GetTasksFilterDto {
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsEnum(TaskStatus, {
+    message: (validationArguments: ValidationArguments) => {
+      return `"${validationArguments.value}" is not a valid status`;
+    },
+  })
   status: TaskStatus;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
