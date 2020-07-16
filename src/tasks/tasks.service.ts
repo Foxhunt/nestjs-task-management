382e3186dae239a874727b8d9f871cc9d2f08a86
Marco <marco@mojica.de>
2020-07-16 15:46:49 +0200

    Add exception to getTaskById

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -33,7 +33,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`No Task with id: "${id}"`);
+    }
+
+    return found;
   }
 
   createTask({ title, description }: CreateTaskDto): Task {
