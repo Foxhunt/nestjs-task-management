57cb5811ae2cbd6e5d59d9652ff9495f7498dca1
Marco <marco@mojica.de>
2020-07-21 22:50:24 +0200

    Add auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private authservice: AuthService) {}
+
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authservice.signup(authCredentialsDto);
+  }
+}
