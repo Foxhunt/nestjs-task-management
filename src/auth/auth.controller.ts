f6360e1a749389e55449e4787ee2e6d827ae3adc
Marco <marco@mojica.de>
2020-07-28 00:24:25 +0200

    Add jwt authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiSecurity, ApiHeader } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RequestBodyObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
 
 @ApiTags('auth')
+@ApiSecurity('Authorisation')
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthService) {}
@@ -16,7 +26,16 @@ export class AuthController {
   }
 
   @Post('/signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authservice.signIn(authCredentialsDto);
+  }
+
+  @Post('/test')
+  @ApiHeader({ name: 'Authorisation' })
+  @UseGuards(AuthGuard())
+  test(@Req() req: RequestBodyObject): void {
+    console.log(req);
   }
 }
